988c37e2053958b47053f55257818353c2a08ef6
Mpholo Leboea <mpholo.leboea@vcontractor.co.za>
2019-07-13 09:00:10 +0200

    Initial project add

---
+++ src/main/java/com/learning/mpholo/sftpetclinic/SftPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.learning.mpholo.sftpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SftPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SftPetClinicApplication.class, args);
+    }
+
+}
