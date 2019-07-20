ab5e953688b7a43e6ee2eb581e36872b22d2fc87
Mpholo Leboea <mpholo.leboea@vcontractor.co.za>
2019-07-20 06:02:11 +0200

    adding base entity. closes #10.

--- pet-clinic-data/src/main/java/com/learning/mpholo/sftpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/com/learning/mpholo/sftpetclinic/model/Person.java
@@ -1,6 +1,6 @@
 package com.learning.mpholo.sftpetclinic.model;
 
-public class Person {
+public class Person extends BaseEntity {
 
     private String firstName;
     private String lastName;
--- pet-clinic-data/src/main/java/com/learning/mpholo/sftpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/com/learning/mpholo/sftpetclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package com.learning.mpholo.sftpetclinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity {
 
     private String name;
 
