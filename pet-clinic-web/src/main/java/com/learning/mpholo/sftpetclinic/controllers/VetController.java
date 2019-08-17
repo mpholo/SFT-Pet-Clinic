bb9f5085d6e8d51402ca4525b49552da787e5d6f
Mpholo Leboea <mpholo.leboea@vcontractor.co.za>
2019-08-17 22:56:03 +0200

    fixed broken links. Closes #45

--- pet-clinic-web/src/main/java/com/learning/mpholo/sftpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/learning/mpholo/sftpetclinic/controllers/VetController.java
@@ -6,7 +6,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/vets")
 public class VetController {
 
     private final VetService vetService;
@@ -16,7 +15,7 @@ public class VetController {
     }
 
 
-    @RequestMapping({"/","","/index","/index.html"})
+    @RequestMapping({"/vets","","/vets/index","/vets/index.html","/vets.html"})
     public String listVets(Model model) {
 
         vetService.findAll().forEach(v-> System.out.println(v.getFirstName()+" "+v.getLastName()));
