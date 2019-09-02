b5734dbdb7912a196437fcd530b519c4879eea1a
Mpholo Leboea <mpholo.leboea@vcontractor.co.za>
2019-09-02 20:24:55 +0200

    added JPA annotation to visit entity, Closes #38

--- pet-clinic-data/src/main/java/com/learning/mpholo/sftpetclinic/model/Visit.java
+++ pet-clinic-data/src/main/java/com/learning/mpholo/sftpetclinic/model/Visit.java
@@ -1,12 +1,20 @@
 package com.learning.mpholo.sftpetclinic.model;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
+@Entity
+@Table(name="visits")
 public class Visit extends BaseEntity {
 
+    @Column(name="date")
     private LocalDate date;
+    @Column(name="description")
     private String description;
-    private Pet pets;
+
+    @ManyToOne
+    @JoinColumn(name="pet_id")
+    private Pet pet;
 
     public LocalDate getDate() {
         return date;
@@ -24,11 +32,11 @@ public class Visit extends BaseEntity {
         this.description = description;
     }
 
-    public Pet getPets() {
-        return pets;
+    public Pet getPet() {
+        return pet;
     }
 
-    public void setPets(Pet pets) {
-        this.pets = pets;
+    public void setPets(Pet pet) {
+        this.pet = pet;
     }
 }
