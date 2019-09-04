203ac00c69b5713e60cf89d25f63fe43b8f6e4f5
Mpholo Leboea <mpholo.leboea@vcontractor.co.za>
2019-09-04 22:37:13 +0200

    implemented Map impl for Visit Service. Closes #46

---
+++ pet-clinic-data/src/main/java/com/learning/mpholo/sftpetclinic/services/VisitService.java
@@ -0,0 +1,6 @@
+package com.learning.mpholo.sftpetclinic.services;
+
+import com.learning.mpholo.sftpetclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit,Long> {
+}
---
+++ pet-clinic-data/src/main/java/com/learning/mpholo/sftpetclinic/services/map/VisitMapService.java
@@ -0,0 +1,18 @@
+package com.learning.mpholo.sftpetclinic.services.map;
+
+import com.learning.mpholo.sftpetclinic.model.Visit;
+import com.learning.mpholo.sftpetclinic.services.VisitService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class VisitMapService extends AbstractMapService<Visit,Long> implements VisitService {
+
+    @Override
+    public Visit save(Visit visit) {
+       if(visit.getPet()==null || visit.getPet().getOwner()==null ||
+         visit.getPet().getId()==null || visit.getPet().getOwner().getId() == null) {
+           throw  new RuntimeException("Invalid Visit");
+       }
+        return super.save(visit);
+    }
+}
